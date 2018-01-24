9b55acbfa8ef503f5d266f830c7d705edb3d7b07
Lucas Alves Martins <lucasalvesmartins18@gmail.com>
2018-01-24 18:50:55 -0200

    Dockerfile update

--- build/dbconfig.inc.php
+++ build/dbconfig.inc.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_NAME", getenv('ocsweb'));
-define("SERVER_READ", getenv('db-ocs'));
-define("SERVER_WRITE", getenv('db-ocs'));
+define("SERVER_READ", getenv('db'));
+define("SERVER_WRITE", getenv('db'));
 define("COMPTE_BASE", getenv('ocs'));
 define("PSWD_BASE", getenv('ocs'));
 ?>
