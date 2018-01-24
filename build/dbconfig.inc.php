037a5f8c9feb92c3eeb27395569fbc6ce95714e1
Lucas Alves Martins <lucasalvesmartins18@gmail.com>
2018-01-24 17:11:10 -0200

    Dockerfile update

--- build/dbconfig.inc.php
+++ build/dbconfig.inc.php
@@ -4,5 +4,4 @@ define("SERVER_READ", getenv('db-ocs'));
 define("SERVER_WRITE", getenv('db-ocs'));
 define("COMPTE_BASE", getenv('ocs'));
 define("PSWD_BASE", getenv('ocs'));
-$_SESSION["PSWD_BASE"]=PSWD_BASE;
 ?>
