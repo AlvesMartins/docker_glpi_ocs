b88fd607b7ee773f9ffd0306b848afae996b7ccd
Lucas Alves Martins <lucasalvesmartins18@gmail.com>
2018-01-26 08:58:12 -0200

    All conf update

--- build/dbconfig.inc.php
+++ build/dbconfig.inc.php
@@ -1,7 +1,9 @@
+
 <?php
-define("DB_NAME", getenv('ocsweb'));
-define("SERVER_READ", getenv('db'));
-define("SERVER_WRITE", getenv('db'));
-define("COMPTE_BASE", getenv('ocs'));
-define("PSWD_BASE", getenv('ocs'));
+define("DB_NAME", getenv('OCS_DBNAME'));
+define("SERVER_READ", getenv('OCS_DBSERVER_READ'));
+define("SERVER_WRITE", getenv('OCS_DBSERVER_WRITE'));
+define("COMPTE_BASE", getenv('OCS_DBUSER'));
+define("PSWD_BASE", getenv('OCS_DBPASS'));
+$_SESSION["PSWD_BASE"]=PSWD_BASE;
 ?>
