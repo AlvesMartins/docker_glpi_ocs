5150f44ea7510f22f5f299579a11521f7477bdeb
Lucas Alves Martins <lucasalvesmartins18@gmail.com>
2018-01-31 12:22:18 -0200

    update script

---
+++ clean_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo docker ps
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm  $(sudo docker ps -a -q)
+sudo docker rmi $(sudo docker images -q)
+sudo docker system prune 
+sudo docker image  prune 
+sudo docker volume  prune 
+sudo docker volume rm -f $(sudo docker volume ls)
+
