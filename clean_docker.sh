43a9829377c5cc87e8fb759633dfcd988a14759d
AlvesMartins <lucasalvesmartins18@gmail.com>
2018-02-06 16:08:40 -0200

    update phpmy

--- clean_docker.sh
+++ clean_docker.sh
@@ -6,6 +6,6 @@ sudo docker rm  $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -q)
 sudo docker system prune 
 sudo docker image  prune 
-sudo docker volume  prune 
-sudo docker volume rm -f $(sudo docker volume ls)
+#sudo docker volume  prune 
+#sudo docker volume rm -f $(sudo docker volume ls)
 
